b41e49325e923c321b96d50562084af2ccb146fb
Katelyn0304 <katelyn20030304@gmail.com>
2022-05-06 01:49:08 +0800

    validate

---
+++ node/express/validation.js
@@ -0,0 +1,43 @@
+const Joi = require('joi');
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+];
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+app.get('/api/courses', (req, res) => {
+    res.send(courses);
+});
+
+app.post('/api/courses', (req, res) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    const result = Joi.valid(req.body, schema);
+    console.log(result);
+
+    if (result.error) {
+        // 400 bad request
+        res.status(400).send(result.error);
+        return; // What does return mean ?
+    }
+
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+app.listen(3000, () => console.log('Listening on port 3000...'));
