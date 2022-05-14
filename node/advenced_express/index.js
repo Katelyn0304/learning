151ddf07588c98c3fba933c41d8974215e2bc02a
Katelyn0304 <katelyn20030304@gmail.com>
2022-05-14 19:10:02 +0800

    middleware

--- node/advenced_express/index.js
+++ node/advenced_express/index.js
@@ -5,16 +5,32 @@ const authenticate = require('./middleware/authenticate');
 
 // express.json() is a middleware function just like logger, its architecture is also like logger.
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 // Speaking of middleware function, there are two middleware functions we had already seen.
 // One is app.use(express.json()), and the other is (req, res) =>
 
 app.use(logger);
-
 app.use(authenticate);
 
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+];
+
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(courses);
+});
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
 });
 
 app.listen(3000, () => console.log(`Listening on port 3000...`));
