99a16878bb6ea1b05e1cc72fdb105cf655e27d50
Katelyn0304 <katelyn20030304@gmail.com>
2022-05-14 19:51:18 +0800

    index.js

--- node/advenced_express/index.js
+++ node/advenced_express/index.js
@@ -1,27 +1,28 @@
 const express = require("express");
+const app = express();
 const helmet = require('helmet');
 const morgan = require('morgan');
-const app = express();
 const logger = require('./middleware/logger');
 const authenticate = require('./middleware/authenticate');
 
+// Environment
 console.log(`NODE_ENV: ${process.env.NODE_ENV}`); // this will stand undefined if we don't set the environment
 console.log(`app: ${app.get('env')}`); // this will return development by defult
-
-// express.json() is a middleware function just like logger, its architecture is also like logger.
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(helmet());
-//app.use(morgan('tiny'));
 
 if (app.get('env') === 'development') {
     app.use(morgan('tiny'));
     console.log('Morgan enabled...'); // if we set NODE_ENV=production, it won't log on console.
 }
 
+// Middleware function
 // Speaking of middleware function, there are two middleware functions we had already seen.
 // One is app.use(express.json()), and the other is (req, res) =>
+// express.json() is a middleware function just like logger, its architecture is also like logger.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(helmet());
+app.use(morgan('tiny'));
 
 app.use(logger);
 app.use(authenticate);
