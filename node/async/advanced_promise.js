ed486b9ff7313e0b117734e240f26c062089c6e4
Katelyn0304 <katelyn20030304@gmail.com>
2022-05-25 17:50:58 +0800

    async

--- node/async/advanced_promise.js
+++ node/async/advanced_promise.js
@@ -1,2 +1,29 @@
-const p = Promise.resolve({id: 1});
-p.then(result => console.log(result));
+const p_resolve = Promise.resolve({id: 1});
+p_resolve.then(result => console.log(result));
+
+const p_reject = Promise.reject(new Error('reason for rejection...'));
+p_reject.catch(error => console.log(error));
+
+/*------------------------------------------------------------------------------*/
+
+// This is what the real asynchronous does.
+// When the waiter get an order from p1 and send it into kitchen, he immediately goes to p2.
+
+const p1 = new Promise((resolve) => {
+    setTimeout(() => {
+        console.log('Async operation 1...');
+        resolve(1);
+    }, 2000);
+});
+
+const p2 = new Promise((resolve) => {
+    setTimeout(() => {
+        console.log('Async operation 2...');
+        resolve(2);
+    }, 2000); // Try changing 2000 to 4000
+});
+
+// When all the promises are resolved in the array, it will create a new promise.
+// Then try changing all to race, new promise will be built when one of the promises was done.
+Promise.all([p1, p2])
+    .then(result => console.log(result));
---
+++ node/async/async&await.js
@@ -0,0 +1,40 @@
+async function displayCommits() {
+    try {
+        const user = await getUser(1);
+        const repos = await getRepositories(user.gitHubUsername);
+        const commits = await getCommits(repos[0]);
+        console.log(commits);
+    }
+    catch (err) {
+        console.log('Error', err.message)
+    }
+}
+displayCommits();
+
+function getUser(id) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Reading a user from a database...');
+            resolve({ id: id, gitHubUsername: 'Mosh' });
+        }, 2000);
+    });
+}
+
+function getRepositories(username) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Calling GitHub API...');
+            // resolve(['repo1', 'repo2', 'repo3']);
+            reject(new Error('Could not get the repos.'));
+        }, 2000);
+    });
+}
+
+function getCommits(repo) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Calling GitHub API...');
+            resolve(['commits']);
+        }, 2000);
+    });
+}
