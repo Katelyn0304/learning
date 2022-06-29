f7438a39c6645f28e6ef39fa9a066743a7ef3726
Katelyn0304 <katelyn20030304@gmail.com>
2022-06-29 22:03:19 +0800

    user

--- node/mongodb/app/routes/users.js
+++ node/mongodb/app/routes/users.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
+// To advanced password validation, can see in joi password complexity
+
 router.post('/', async (req, res) => {
   const { error } = schema.validate(_.pick(req.body, ['name', 'email', 'password']));
   if (error) return res.status(400).send(error.details[0].message);
