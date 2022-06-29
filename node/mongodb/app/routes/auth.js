9e183088fc44d1c4c793cecd41020c5f8c77fb5e
Katelyn0304 <katelyn20030304@gmail.com>
2022-06-29 22:53:50 +0800

    jsonwebtoken

---
+++ node/mongodb/app/routes/auth.js
@@ -0,0 +1,27 @@
+const {User} = require('../models/user');
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const express = require('express');
+const Joi = require('joi')
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+  const { error } = schema.validate(_.pick(req.body, ['email', 'password']));
+  if (error) return res.status(400).send(error.details[0].message);
+  
+  let user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send('Invalid email or password.');
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send('Invalid email or password.');
+
+  res.send(true);
+});
+
+const schema = Joi.object({
+    email: Joi.string().min(5).max(255).required().email(),
+    password: Joi.string().min(5).max(255).required()
+});
+
+module.exports = router;
