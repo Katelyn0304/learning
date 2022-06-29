f5c129b749c34984038da62bc91e750329a6b00a
Katelyn0304 <katelyn20030304@gmail.com>
2022-06-30 01:53:49 +0800

    config

--- node/mongodb/app/index.js
+++ node/mongodb/app/index.js
@@ -1,3 +1,4 @@
+const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
@@ -9,6 +10,12 @@ const users = require('./routes/users');
 const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
+
+if (!config.get('jwtPrivateKey')) {
+  console.error('FATAL ERROR: jwtPrivateKey is not defined.');
+  process.exit(1);
+  // process is a global object. 0 represent success, anything but 0 represent failure.
+}
 
 mongoose.connect('mongodb://localhost/vidly')
   .then(() => console.log('Connected to MongoDB...'))
--- node/mongodb/app/routes/auth.js
+++ node/mongodb/app/routes/auth.js
@@ -2,6 +2,7 @@ const {User} = require('../models/user');
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const config = require('config');
 const mongoose = require('mongoose');
 const express = require('express');
 const Joi = require('joi')
@@ -17,7 +18,7 @@ router.post('/', async (req, res) => {
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword) return res.status(400).send('Invalid email or password.');
 
-  token = jwt.sign({ _id: user._id }, 'jwt');
+  token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
   res.send(token);
 });
 
