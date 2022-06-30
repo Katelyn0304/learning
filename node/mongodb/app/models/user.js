6a8c2c449576f576c6f8025fbbecbf52d975dd58
Katelyn0304 <katelyn20030304@gmail.com>
2022-06-30 17:26:48 +0800

    10.17

--- node/mongodb/app/models/user.js
+++ node/mongodb/app/models/user.js
@@ -2,6 +2,7 @@ const Joi = require('joi');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const { boolean } = require('joi');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,12 +23,13 @@ const userSchema = new mongoose.Schema({
     required: true,
     minlength: 5,
     maxlength: 1024
-  }
+  },
+  isAdmin: Boolean
 })
 
 // create a method in userSchema object
 userSchema.methods.generateAuthToken = function() {
-  const token = jwt.sign({ _id: this._id }, config.get('jwtPrivateKey'));
+  const token = jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, config.get('jwtPrivateKey'));
   return token;
 }
 
