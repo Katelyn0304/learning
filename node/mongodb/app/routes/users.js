bb0c9242aada39e6c5fcac7fccf491f9134a0d98
Katelyn0304 <katelyn20030304@gmail.com>
2022-06-30 14:45:34 +0800

    10.12

--- node/mongodb/app/routes/users.js
+++ node/mongodb/app/routes/users.js
@@ -1,8 +1,6 @@
 const {User, schema} = require('../models/user');
 const _ = require('lodash');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const config = require('config');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
@@ -13,7 +11,7 @@ router.post('/', async (req, res) => {
   const { error } = schema.validate(_.pick(req.body, ['name', 'email', 'password']));
   if (error) return res.status(400).send(error.details[0].message);
   
-  let user = await User.findOne({ email: req.body.email });
+  let user = await User.findOne({ email: req.body.email }); // if !user, user=null
   if (user) return res.status(400).send('User already registered.');
 
   user = new User(_.pick(req.body, ['name', 'email', 'password']));
@@ -21,7 +19,7 @@ router.post('/', async (req, res) => {
   user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
-  const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
+  const token = user.generateAuthToken();
   res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
 });
 
